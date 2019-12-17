f23679b9d5fb43e477d1df6c6677635a1a6cec52
Daniel <daniel.gonvie@gmail.com>
2019-12-17 10:14:27 +0100

    Deploy to heroku

---
+++ server/public/precache-manifest.63612b71183f35088ddccbd53bb69c25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1225a989853b64554b1a15187bdbd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b67fc634b599a025a371",
+    "url": "/static/css/2.cd317149.chunk.css"
+  },
+  {
+    "revision": "eaf2c22556ece9f18bf6",
+    "url": "/static/css/main.f5eaa08b.chunk.css"
+  },
+  {
+    "revision": "b67fc634b599a025a371",
+    "url": "/static/js/2.e4fd4572.chunk.js"
+  },
+  {
+    "revision": "db9304231320fd14998b316aa1701abc",
+    "url": "/static/js/2.e4fd4572.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eaf2c22556ece9f18bf6",
+    "url": "/static/js/main.78acb541.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
