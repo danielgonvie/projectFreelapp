8c1afd2a83e72fc0429ca11b9c90e6ec32711969
Daniel <daniel.gonvie@gmail.com>
2020-01-13 17:30:38 +0100

    Signup again

--- client/src/components/SignupArtist/SignupArtist.js
+++ client/src/components/SignupArtist/SignupArtist.js
@@ -98,6 +98,114 @@ export default class SignUp extends Component {
     const soundcloud = this.state.social.soundcloud ;
     const github = this.state.social.github ;
 
+    let categoryDisplay = <React.Fragment>
+      <div className="category-container">
+            <div className="category-unit">
+            <input type="radio" className="selected-category" name="category" value="photo" onChange={this.handleChange} /> 
+            <img className="logo-img2" alt="Missing smthing" src="/img/camera.png"></img>
+            </div>
+            <div className="category-unit">
+            <input type="radio" className="selected-category" name="category" value="tattoo" onChange={this.handleChange}/> 
+            <img className="logo-img2" alt="Missing smthing" src="/img/tattoo-machine.png"></img>
+            </div>
+            <div className="category-unit">
+  <input type="radio" className="selected-category" name="category" value="design" onChange={this.handleChange}/> 
+  <img className="logo-img2" alt="Missing smthing" src="/img/creativity.png"></img> 
+  </div>
+  <div className="category-unit">
+  <input type="radio" className="selected-category" name="category" value="music" onChange={this.handleChange}/> 
+  <img className="logo-img2" alt="Missing smthing" src="/img/musical-note.png"></img>
+  </div>
+            </div>
+    </React.Fragment>
+  if (this.state.category === "photo") {
+    categoryDisplay = <React.Fragment>
+    <div className="category-container">
+          <div className="category-unit2">
+          <input type="radio" className="selected-category" name="category" value="photo" onChange={this.handleChange} /> 
+          <img className="logo-img2" alt="Missing smthing" src="/img/camera.png"></img>
+          </div>
+          <div className="category-unit">
+          <input type="radio" className="selected-category" name="category" value="tattoo" onChange={this.handleChange}/> 
+          <img className="logo-img2" alt="Missing smthing" src="/img/tattoo-machine.png"></img>
+          </div>
+          <div className="category-unit">
+<input type="radio" className="selected-category" name="category" value="design" onChange={this.handleChange}/> 
+<img className="logo-img2" alt="Missing smthing" src="/img/creativity.png"></img> 
+</div>
+<div className="category-unit">
+<input type="radio" className="selected-category" name="category" value="music" onChange={this.handleChange}/> 
+<img className="logo-img3" alt="Missing smthing" src="/img/musical-note.png"></img>
+</div>
+          </div>
+  </React.Fragment>
+  } else if (this.state.category === "tattoo") {
+    categoryDisplay = <React.Fragment>
+    <div className="category-container">
+          <div className="category-unit">
+          <input type="radio" className="selected-category" name="category" value="photo" onChange={this.handleChange} /> 
+          <img className="logo-img2" alt="Missing smthing" src="/img/camera.png"></img>
+          </div>
+          <div className="category-unit2">
+          <input type="radio" className="selected-category" name="category" value="tattoo" onChange={this.handleChange}/> 
+          <img className="logo-img2" alt="Missing smthing" src="/img/tattoo-machine.png"></img>
+          </div>
+          <div className="category-unit">
+<input type="radio" className="selected-category" name="category" value="design" onChange={this.handleChange}/> 
+<img className="logo-img2" alt="Missing smthing" src="/img/creativity.png"></img> 
+</div>
+<div className="category-unit">
+<input type="radio" className="selected-category" name="category" value="music" onChange={this.handleChange}/> 
+<img className="logo-img3" alt="Missing smthing" src="/img/musical-note.png"></img>
+</div>
+          </div>
+  </React.Fragment>
+  } else if (this.state.category === "design") {
+    categoryDisplay = <React.Fragment>
+    <div className="category-container">
+          <div className="category-unit">
+          <input type="radio" className="selected-category" name="category" value="photo" onChange={this.handleChange} /> 
+          <img className="logo-img2" alt="Missing smthing" src="/img/camera.png"></img>
+          </div>
+          <div className="category-unit">
+          <input type="radio" className="selected-category" name="category" value="tattoo" onChange={this.handleChange}/> 
+          <img className="logo-img2" alt="Missing smthing" src="/img/tattoo-machine.png"></img>
+          </div>
+          <div className="category-unit2">
+<input type="radio" className="selected-category" name="category" value="design" onChange={this.handleChange}/> 
+<img className="logo-img2" alt="Missing smthing" src="/img/creativity.png"></img> 
+</div>
+<div className="category-unit">
+<input type="radio" className="selected-category" name="category" value="music" onChange={this.handleChange}/> 
+<img className="logo-img3" alt="Missing smthing" src="/img/musical-note.png"></img>
+</div>
+          </div>
+  </React.Fragment>
+  } else if (this.state.category === "music") {
+    categoryDisplay = <React.Fragment>
+    <div className="category-container">
+          <div className="category-unit">
+          <input type="radio" className="selected-category" name="category" value="photo" onChange={this.handleChange} /> 
+          <img className="logo-img2" alt="Missing smthing" src="/img/camera.png"></img>
+          </div>
+          <div className="category-unit">
+          <input type="radio" className="selected-category" name="category" value="tattoo" onChange={this.handleChange}/> 
+          <img className="logo-img2" alt="Missing smthing" src="/img/tattoo-machine.png"></img>
+          </div>
+          <div className="category-unit">
+<input type="radio" className="selected-category" name="category" value="design" onChange={this.handleChange}/> 
+<img className="logo-img2" alt="Missing smthing" src="/img/creativity.png"></img> 
+</div>
+<div className="category-unit2">
+<input type="radio" className="selected-category" name="category" value="music" onChange={this.handleChange}/> 
+<img className="logo-img3" alt="Missing smthing" src="/img/musical-note.png"></img>
+</div>
+          </div>
+  </React.Fragment>
+  }
+
+
+
     return (
       <div className="signup-artist-page">
         <Link className="back-link bounce" to="/">
@@ -105,7 +213,7 @@ export default class SignUp extends Component {
         </Link>
         {/* <div className="signup-box"> */}
           <h1 className="title">SignUp</h1>
-          <form className="signup-fields" onSubmit={this.handleSignUp}>
+          <form className="signup-fields2" onSubmit={this.handleSignUp}>
           <div className="diapositive">
             <label className="login-labels" htmlFor="email">
               Email:
@@ -117,6 +225,7 @@ export default class SignUp extends Component {
               value={email}
               required
               onChange={this.handleChange}
+              placeholder="Correo para entrar"
             />
             <label className="login-labels" htmlFor="password">
               Password:
@@ -127,6 +236,7 @@ export default class SignUp extends Component {
               value={password}
               name="password"
               required
+              placeholder="Contraseña"
               onChange={this.handleChange}
             />
             <div className="upload-btn-wrapper">
@@ -135,19 +245,21 @@ export default class SignUp extends Component {
                 className="signup-upload"
                 type="file"
                 name="picture"
+                
                 onChange={this.handleUpload}
               />
             </div>
             </div>
 <div className="diapositive">
              <label className="login-labels" htmlFor="name">
-              name:
+              Nombre:
             </label>
             <input
               className="login-field"
               type="text"
               value={name}
               name="name"
+              placeholer="Nombre público"
               onChange={this.handleChange}
             />
             <label className="login-labels" htmlFor="alias">
@@ -158,24 +270,33 @@ export default class SignUp extends Component {
               type="text"
               value={alias}
               name="alias"
+              placeholer="Alias público"
               onChange={this.handleChange}
             />
             <label className="login-labels" htmlFor="toggleAlias">
               Mostrar nombre o alias:
             </label>
-            <input type="radio" name="toggleAlias" value="name" onChange={this.handleChange} /> Nombre
-  <input type="radio" name="toggleAlias" value="alias" onChange={this.handleChange}/> Alias
-  <input type="radio" name="toggleAlias" value="both" onChange={this.handleChange}/> Ambos
-
+            <div className="toggleAlias-selected">
+            <div>
+            <input type="radio" name="toggleAlias" value="name" onChange={this.handleChange} /> {name}
             </div>
-            <div className="diapositive">
+            <div>
+            <input type="radio" name="toggleAlias" value="alias" onChange={this.handleChange}/> {alias}
+            </div>
+            <div>
+            <input type="radio" name="toggleAlias" value="both" onChange={this.handleChange}/> {name}, {alias}
+            </div>
+            </div>
+            </div>
+            <div className="diapositive-category">
             <label className="login-labels" htmlFor="category">
               Categoría:
             </label>
-            <input type="radio" name="category" value="photo" onChange={this.handleChange} /> Foto
-  <input type="radio" name="category" value="tattoo" onChange={this.handleChange}/> Tattoo
-  <input type="radio" name="category" value="design" onChange={this.handleChange}/> Diseño
-  <input type="radio" name="category" value="music" onChange={this.handleChange}/> Música
+            
+            {categoryDisplay}
+
+
+
 
             <label className="login-labels" htmlFor="subcategory">
               Subcategoría:
@@ -185,7 +306,7 @@ export default class SignUp extends Component {
               type="text"
               value={subcategory}
               name="subcategory"
-              placeholder="En qué te especializas"
+              placeholder="Especialización"
               onChange={this.handleChange}
             />
 </div>
@@ -198,6 +319,7 @@ export default class SignUp extends Component {
               type="text"
               value={city}
               name="city"
+              placeholder="Ciudad en la que trabajas"
               onChange={this.handleChange2}
             />
             <label className="login-labels" htmlFor="country">
@@ -208,6 +330,7 @@ export default class SignUp extends Component {
               type="text"
               value={country}
               name="country"
+              placeholder="País en el que trabajas"
               onChange={this.handleChange2}
             />
             <label className="login-labels" htmlFor="availability">
@@ -222,70 +345,76 @@ export default class SignUp extends Component {
             </div>
             <div className="diapositive">
             <label className="login-labels" htmlFor="contactEmail">
-              contactEmail:
+              Email de contacto:
             </label>
             <input
               className="login-field"
               type="text"
               value={contactEmail}
               name="contactEmail"
+              placeholder="Email público de contacto"
               onChange={this.handleChange}
             />
             <label className="login-labels" htmlFor="contactPhone">
-              contactPhone:
+              Teléfono de contacto:
             </label>
             <input
               className="login-field"
               type="text"
               value={contactPhone}
               name="contactPhone"
+              placeholder="Teléfono público de contacto"
               onChange={this.handleChange}
             />
             <label className="login-labels" htmlFor="instagram">
-              instagram:
+              Instagram:
             </label>
             <input
               className="login-field"
               type="text"
               value={instagram}
               name="instagram"
+              placeholder="@usuario"
               onChange={this.handleChange3}
             />
                         <label className="login-labels" htmlFor="soundcloud">
-              soundcloud:
+              Soundcloud:
             </label>
             <input
               className="login-field"
               type="text"
               value={soundcloud}
               name="soundcloud"
+              placeholder="Usuario soundcloud"
               onChange={this.handleChange3}
             />
                         <label className="login-labels" htmlFor="github">
-              github:
+              Github:
             </label>
             <input
               className="login-field"
               type="text"
               value={github}
               name="github"
+              placeholder="Usuario github"
               onChange={this.handleChange3}
             />
                         <label className="login-labels" htmlFor="other">
-              other:
+              Página web:
             </label>
             <input
               className="login-field"
               type="text"
               value={other}
               name="other"
+              placeholder="Página web/otros"
               onChange={this.handleChange3}
             />
 </div>
 
             
             <input
-              className="signup-button"
+              className="signup-button2"
               type="submit"
               value="Create account"
             />
